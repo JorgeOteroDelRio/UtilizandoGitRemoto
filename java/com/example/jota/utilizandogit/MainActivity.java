a4ff41248bc81e539fc0d47324b48790a29df4f9
Jorge <joterodelrio@danielcastelao.org>
2016-11-02 09:11:10 +0100

    Inicio de proyecto

---
+++ java/com/example/jota/utilizandogit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jota.utilizandogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.hmundo);
+    }
+
+    void eventoBoton(View v){
+        tv.setText(R.string.hmundo);
+    }
+}
